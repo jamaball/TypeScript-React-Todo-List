55589013a689ce0d50c5e09cf8bf45fff251f605
jamaball <104403889+jamaball@users.noreply.github.com>
2022-12-29 16:46:35 -0500

    Initial Commut
    first commit, just setting up React w Typescript and messing with some inputs and such

---
+++ todo-list/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Todility</p>
+          An app by James Ball
+      </header>
+      <input placeholder="Enter the items you would like to accomplish here">
+        
+      </input>
+    </div>
+  );
+}
+
+export default App;
